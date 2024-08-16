1cf0bcdb6eff241d1d97623ee9764c5917bdb43b
Finn Lestrange <finn.lestrange@gmail.com>
2024-08-16 13:38:54 +0100

    added plugin configuration & sql queries

--- ISA HSST/uploadCSVs.bat
+++ ISA HSST/uploadCSVs.bat
@@ -1,2 +1,14 @@
 cd D:\GAMADV-XTD3
-.\gam.exe user gveitch@isa.aberdeen.sch.uk update drivefile 1eZcoamEcugJNRFcf8Y4rIuuCTWemKZpRHL6sOYdcZEQ csvsheet id:423274577 localfile D:\PSShare\external\autosend\HSSTstoredgradesexport retainname
+D:\GAMADV-XTD3\gam.exe user gveitch@isa.aberdeen.sch.uk update drivefile 1eZcoamEcugJNRFcf8Y4rIuuCTWemKZpRHL6sOYdcZEQ csvsheet id:423274577 localfile D:\PSShare\external\autosend\HSST\HSSTstoredgradesexport retainname
+echo "Uploaded Stored Grades Sheet"
+
+D:\GAMADV-XTD3\gam.exe user gveitch@isa.aberdeen.sch.uk update drivefile 1eZcoamEcugJNRFcf8Y4rIuuCTWemKZpRHL6sOYdcZEQ csvsheet id:381211607 localfile D:\PSShare\external\autosend\HSST\HSSTcemtestexport retainname
+echo "Uploaded CEM Test Results" 
+
+D:\GAMADV-XTD3\gam.exe user gveitch@isa.aberdeen.sch.uk update drivefile 1eZcoamEcugJNRFcf8Y4rIuuCTWemKZpRHL6sOYdcZEQ csvsheet id:1159097062 localfile D:\PSShare\external\autosend\HSST\HSSTigcseexport retainname
+echo "Uploaded IGCSE Results" 
+
+D:\GAMADV-XTD3\gam.exe user gveitch@isa.aberdeen.sch.uk update drivefile 1eZcoamEcugJNRFcf8Y4rIuuCTWemKZpRHL6sOYdcZEQ csvsheet id:912099034 localfile D:\PSShare\external\autosend\HSST\HSSTmaptestexport retainname
+echo "Uploaded Map Test Scores"
+
+echo "Script Finished."
